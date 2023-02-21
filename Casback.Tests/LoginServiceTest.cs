03c680e17f5616a40ab4746323275e0aa458bf01
Victor Santos <victorluizsantos49@gmail.com>
2023-02-21 20:07:46 -0300

    teste do login feito

--- Casback.Tests/LoginServiceTest.cs
+++ Casback.Tests/LoginServiceTest.cs
@@ -1,3 +1,4 @@
+using Cashback.Application.Extensions;
 using Cashback.Application.Services;
 using Cashback.Domain.Entities;
 using Cashback.Repository.Interfaces;
@@ -7,24 +8,53 @@ namespace Casback.Tests
 {
     public class LoginServiceTest
     {
-        private readonly LoginService _loginService;
-        private readonly Mock<IBaseRepository<UserEntity>> mock = new Mock<IBaseRepository<UserEntity>>();
+        private readonly Mock<IBaseRepository<UserEntity>> _userRepositoryMock = new Mock<IBaseRepository<UserEntity>>();
+        private readonly LoginService _service;
 
         public LoginServiceTest()
         {
-            _loginService = new(mock.Object);
+            _service = new(_userRepositoryMock.Object);
+        }
+
+
+        [Fact]
+        public void ExecuteLogin_ShouldFail_WhenUserNotFound()
+        {
+            //Arrange
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(new List<UserEntity>());
+
+
+            //Act
+            var actual = _service.ExecuteLogin(null, null);
+
+            var expected = BaseDtoExtension.NotFound();
+
+
+            //Assert
+            Assert.Equal(expected._Message, actual._Message);
+            Assert.Equal(expected._StatusCode, actual._StatusCode);
         }
 
         [Fact]
-        public void login()
+        public void ExecuteLogin_ShouldBeSuccess_WhenUserExists()
         {
+            //Arrange
             var users = new List<UserEntity>();
 
-            mock.Setup(x => x.GetAll()).Returns(users);
+            var user = new UserEntity("carlos", "carlos@gmail.com", "carlos", null);
 
-            var actual = _loginService.ExecuteLogin("pedro@gmail.com","pedro");
+            users.Add(user);
 
-            Assert.Equal(404, actual._StatusCode);
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(users);
+
+            //Act
+            var actual = _service.ExecuteLogin(user.Email, user.Password);
+
+            var expected = BaseDtoExtension.Create(200, "Login executado", true, user.Id);
+
+            //Assert
+            Assert.Equal(expected._Message, actual._Message);
+            Assert.Equal(expected._StatusCode, actual._StatusCode);
         }
     }
 }
